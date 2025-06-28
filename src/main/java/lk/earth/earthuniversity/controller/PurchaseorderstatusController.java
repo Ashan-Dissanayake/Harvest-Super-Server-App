f2fe1d40e9e01f32c747bb784dd601a7a5271026
Ashan Dissanayake <ashanpathum899@gmail.com>
2025-06-28 10:40:47 +0530

    Implement payment controller & postatus controllers

---
+++ src/main/java/lk/earth/earthuniversity/controller/PurchaseorderstatusController.java
@@ -0,0 +1,40 @@
+package lk.earth.earthuniversity.controller;
+
+import lk.earth.earthuniversity.dao.PurchaseorderstatusDao;
+import lk.earth.earthuniversity.entity.Purchaseorderstatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@CrossOrigin
+@RestController
+@RequestMapping(value = "/purchaseorderstatuses")
+public class PurchaseorderstatusController {
+
+    @Autowired
+    private PurchaseorderstatusDao purchaseorderstatusdao;
+
+    @GetMapping(path ="/list", produces = "application/json")
+    public List<Purchaseorderstatus> get() {
+
+        List<Purchaseorderstatus> purchaseorderstatuses = this.purchaseorderstatusdao.findAll();
+
+        purchaseorderstatuses = purchaseorderstatuses.stream().map(
+                purchaseorderstatus -> { Purchaseorderstatus i = new Purchaseorderstatus();
+                    i.setId(purchaseorderstatus.getId());
+                    i.setName(purchaseorderstatus.getName());
+                    return i; }
+        ).collect(Collectors.toList());
+
+        return purchaseorderstatuses;
+
+    }
+
+}
+
+
---
+++ src/main/java/lk/earth/earthuniversity/dao/PurchaseorderstatusDao.java
@@ -0,0 +1,11 @@
+package lk.earth.earthuniversity.dao;
+
+import lk.earth.earthuniversity.entity.Purchaseorderstatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PurchaseorderstatusDao extends JpaRepository<Purchaseorderstatus,Integer> {
+
+
+}
+
